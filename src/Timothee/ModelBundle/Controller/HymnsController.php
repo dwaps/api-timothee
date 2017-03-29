277dc610c71b20e1b5d2b47ff6d02da793b0c110
Michael Cornillon <contact@dwaps.fr>
2017-03-29 21:14:08 +0200

    Gestion ajout chants en bdd; 74 chants en bdd

--- src/Timothee/ModelBundle/Controller/HymnsController.php
+++ src/Timothee/ModelBundle/Controller/HymnsController.php
@@ -10,14 +10,597 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Timothee\ModelBundle\Entity\Hymn;
 
+
 class HymnsController extends Controller
 {
+
+    private $hymns = "
+
+
+51
+Souviens-toi
+Ec 12
+\tSouviens-toi, souviens-toi de ton Créateur !
+\tSouviens-toi de ton Créateur pendant les jours de ta jeunesse,
+\tAvant que la poussière retourne à la terre comme elle y était.
+\tSouviens-toi, souviens-toi de ton Créateur !
+\tSouviens-toi de ton Créateur !
+\tAvant que l'esprit ne retourne à Dieu qui l'a donné.
+
+Réjouis-toi, réjouis-toi et marche dans les voies de ton cœur !
+Réjouis-toi, réjouis-toi, va selon les regards de tes yeux !
+Mais sache, mais sache que Dieu te jugera. (bis)
+
+
+52
+Si quelqu'un est en christ
+2 Co 5;Cl 1
+\tSi quelqu'un est en Christ
+\tIl est une nouvelle création.
+\tLes choses anciennes sont passées.
+\tToutes choses sont devenues nouvelles.
+
+Car Dieu était en Christ
+Pour réconcilier le monde en offrant sa vie.
+Nous qui étions ennemis de Dieu
+Il nous a réconciliés.
+
+Christ était sans péché.
+Il devint péché pour nous, oui, à cause de nous !
+Pour nous faire paraître devant lui
+Justes, saints et pardonnés.
+
+Tout cela vient de Dieu
+Qui nous a réconciliés avec lui par Christ.
+Qui nous a donné le ministère
+De la réconciliation.
+Qui nous a donné le ministère
+De la réconciliation.
+
+
+53
+Éternel, tu me feras connaitre
+Ps 16
+\tÉternel, tu me feras connaître le sentier de la vie,
+\tDe la vie, de la vie.
+\tDevant toi la joie abonde,
+\tLa joie abonde toujours.
+
+Garde-moi, garde-moi, ô Dieu !
+Car en toi je cherche un refuge.
+Tu es mon Seigneur, mon souverain bien.
+Avec toi je ne craindrai rien.
+
+Je bénis, bénis l'Éternel.
+Même dans la nuit mon cœur m'exhorte.
+J'ai l'Éternel toujours devant les yeux.
+L'Éternel est mon conseiller.
+
+Jamais je ne chancellerai
+Car l'Éternel est à ma droite.
+Ainsi mon esprit est dans l'allégresse,
+Et tout mon cœur est dans la joie.
+
+
+54
+Venez à moi
+Mt 11
+\tVenez à moi, vous qui êtes fatigués !
+\tVenez à moi, vous tous qui êtes chargés !
+\tVenez à moi, vous qui portez le joug,
+\tLe joug de la détresse, le joug de la mort !
+\tVenez à moi, vous qui traînez vos chaînes,
+\tLes chaînes du doute, les chaînes de l'angoisse.
+
+\tFin : \"Venez à moi\", nous dit Jésus. (bis)
+
+\"Venez à moi, nous dit Jésus, 
+Et je vous donnerai du repos,
+(bis)
+Du repos pour vos cœurs meurtris,
+Repos pour vos âmes lassées.\"
+
+Prenez mon joug, car il est doux.
+Recevez toutes mes instructions.
+(bis)
+Mon fardeau est doux et léger.
+Je suis doux et humble de cœur.
+
+
+55
+À celui qui offre
+Ps 50
+\tÀ celui qui offre pour sacrifice
+\tDes actions de louange à son Dieu,
+\tEt qui veille sur sa voie
+\tJe ferai voir le salut de Dieu. (bis)
+
+Au jour de la détresse invoque-moi;
+Je te délivrerai, tu me glorifieras.
+(bis)
+
+Dieu, l’Éternel, nous parle : Rassemblez-vous !
+Au nom du sacrifice qui nous a réunis.
+(bis)
+
+
+56
+Je vous ai aimés
+Ml
+\"Je vous ai aimés\", dit l’Éternel.
+Vous dites : \"En quoi nous as-tu aimés ?\"
+Un fils honore son père et vous êtes rebelles.
+Où est la crainte de mon nom car vous me profanez ?
+Un fils honore son père et vous êtes rebelles.
+Où est la crainte de mon nom car vous me profanez ?
+
+\tPrions Dieu maintenant pour qu'il ait pitié de nous !
+\tÀ nous cet ordre maintenant de donner gloire à son saint nom !
+
+Revenez à moi, dit l’Éternel.
+Vous dites : En quoi faut-il revenir ?
+Voici vous me trompez, moi qui suis l’Éternel.
+Livrez avec joie vos trésors, je veux tous vous bénir.
+Voici vous me trompez, moi qui suis l’Éternel.
+Livrez avec joie vos trésors, je veux tous vous bénir.
+
+Ils seront à moi, dit l’Éternel,
+Tous ceux qui craignent et honorent mon nom.
+Tous ceux qui sont à moi verront ma compassion.
+Ils connaîtront ma justice et ma pleine guérison.
+Tous ceux qui sont à moi verront ma compassion.
+Ils connaîtront ma justice et ma pleine guérison.
+
+
+57
+Jésus-christ est la lumière
+Jn 8
+Jésus-Christ est la lumière du monde;
+Celui qui le suit ne marchera pas dans les ténèbres.
+Oh, oh...
+
+Mais il aura la lumière de la vie;
+Il ne craindra plus la mort, mais il vivra avec Jésus.
+Oh, oh...
+
+Seigneur, tu es la lumière de nos vies.
+Nous Te bénissons, nous t'adorons, Seigneur, notre Sauveur !
+Oh, oh...
+
+Tu as donné la lumière dans ce monde.
+Tu es mort sur la croix en versant ton sang pour nos péchés.
+Oh, oh...
+
+
+58
+Heureux l'homme qui ne marche pas
+Ps 1
+Heureux l'homme qui ne marche pas
+Selon le conseil des méchants,
+Qui ne s'arrête pas sur la voie des pécheurs,
+Ni en compagnie des moqueurs,
+Mais qui trouve son plaisir dans la loi de l'Éternel
+Et qui la médite jour et nuit.
+Il est comme un arbre près des eaux
+Qui donne son fruit en sa saison,
+Dont le feuillage ne se flétrit pas;
+Tout ce qu'il fait lui réussit.
+
+Il n'en est pas ainsi des méchants;
+Comme la paille dissipée au vent.
+Ils ne résistent pas au jour du jugement,
+Ni les pécheurs parmi les justes.
+Mais le Seigneur est avec ceux qui aiment sa loi
+Et qui la méditent jour et nuit.
+La voie des pécheurs mène à la ruine
+Mais le Seigneur connaît leur voie.
+Il connaît aussi la voie du juste
+Et il bénit tout ce qu'il fait.
+
+
+59
+Revenez enfants rebelles
+Jr 3
+Revenez enfants rebelles,
+Mon regard ne sera pas sévère.
+Revenez enfants rebelles,
+Car je suis miséricordieux.
+Oui, nous avons péché contre l'Éternel, (bis)
+Contre l'Éternel !
+
+Revenez enfants rebelles,
+Reconnaissez vos iniquités
+Et ne suivez plus les penchants,
+Les penchants de votre mauvais cœur.
+Oui, nous avons péché contre l'Éternel.
+Nous n'avons pas écouté, écouté sa voix,
+Écouté sa voix !
+
+Revenez enfants rebelles,
+Je pardonnerai vos infidélités.
+Ma colère ne dure pas toujours
+Car je suis, car je suis votre Maître.
+Oui, nous voici Seigneur, nous allons à toi,
+Car tu es l'Éternel, l'Éternel notre Dieu,
+L'Éternel notre Dieu (bis)
+
+
+60
+La bienveillance de l’Éternel remplit la terre
+Ps 33
+La bienveillance de l'Éternel remplit la terre.
+Les cieux ont été faits par sa sainte Parole.
+Il dit et la chose arrive ! il ordonne et elle existe !
+
+\tNotre âme espère en l'Éternel,
+\t(Notre cœur met en lui sa joie, car il est notre bouclier)
+\tIl est notre Rocher suprême.
+\t(Notre âme espère en l'Éternel, il est notre rocher)
+
+Heureux celui que Dieu choisit pour héritier !
+L'Éternel regarde du haut des cieux les hommes,
+Il observe tous les habitants, lui qui leur forme le cœur à tous.
+
+L'œil de l'Éternel est sur tous ceux qui Le craignent
+Afin d'arracher quelques-uns d'eux à la mort
+Et de les faire survivre pendant la grande famine.
+
+
+61
+Et l'Ésprit et l'Épouse
+Ap 22
+\tEt l'Esprit et l'Épouse disent : \"Viens Seigneur !\"
+\t(Reviens, Seigneur !)
+\tQue tous ceux qui entendent disent : \"Viens bientôt !\"
+\t(Reviens bientôt !)
+\tTenons pour certaine sa venue prochaine !
+\t(Reviens, Seigneur !)
+\tParole prophétique, lumière dans la nuit.
+\t(Reviens bientôt !)
+
+Prenez donc garde à vous-mêmes
+De crainte que vos cœurs ne s'endorment
+Par les soucis du manger et du boire !
+
+Ne dormons pas comme les autres;
+Soyons sobres, veillons !
+Car vous le savez bien vous-même qu'il viendra dans la nuit...
+
+Pour avoir de l'assurance
+Demeurez donc en lui,
+Afin que, lorsqu'il paraîtra, vous ne soyez confus !
+
+Ayez entière espérance
+Dans la grâce de Dieu
+Qui vous sera manifestée quand Jésus reviendra.
+
+
+62
+Je vous laisse la paix
+Jn 14;Ph 4;1 Pi 5
+\tJe vous laisse la paix,
+\tJe vous donne ma paix.
+\tJe ne la donne pas comme le monde la donne !
+\tQue votre cœur ne se trouble pas,
+\tQue votre cœur ne s’alarme pas,
+\tCroyez en Dieu ! Croyez en Moi !
+
+Ne vous inquiétez de rien.
+Faites connaître vos besoins.
+Par des prières à Dieu. Et la paix de Dieu
+Gardera vos cœurs, vos pensées en Christ. (bis)
+
+Dieu résiste aux orgueilleux.
+Humiliez-vous sous sa main,
+Et déchargez-vous sur lui de tous vos soucis,
+Car Dieu lui-même prendra soin de vous. (bis)
+
+
+63
+Que Dieu ait pitié de nous
+Ps 67
+Que Dieu ait pitié de nous et que Dieu nous bénisse.
+Qu'il fasse luire sa face, afin que sur la terre
+Nous connaissions sa voie, les nations son salut !
+
+\tLes nations Te louent, ô Dieu, les peuples t'adorent. (bis)
+
+Les nations se réjouissent et sont dans l'allégresse
+Car tu juges les peuples, tu juges avec droiture.
+Tu conduis les nations, toute la terre te craint.
+
+
+64
+Cherchez l'Éternel
+Es 55
+Cherchez l'Éternel pendant qu'il se trouve,
+Et invoquez-le tandis qu'il est près !
+Que le méchant abandonne sa voie
+Et l'homme d'iniquité ses pensées !
+Car vos pensées ne sont pas mes pensées.
+Vos voies ne sont pas celles du Seigneur.
+Mais si vous retournez à l'Éternel,
+Notre Dieu aura pitié de vous.
+
+Comme la pluie ou la neige en hiver
+Descend des cieux et n'y retourne pas
+Sans avoir arrosé, fécondé la terre,
+Donné de la semence au semeur,
+Il en est ainsi de toute ma Parole;
+Elle ne reviendra pas à moi sans effet,
+Sans avoir exécuté ma volonté
+Et avoir accompli mes desseins.
+
+Vous sortirez le cœur rempli de joie,
+Serez conduits sur des chemins de paix.
+Les montagnes éclateront d'allégresse
+Et tous les arbres battront des mains.
+Au lieu de l'épine s'élèvera le cyprès,
+Au lieu de la ronce le myrte grandira.
+Oui, cherchez l'Éternel pendant qu'il se trouve !
+Invoquez-le tandis qu'il est près ! (bis)
+
+
+65
+Il est beau de louer
+Ps 92
+\tIl est beau de louer, de célébrer l'Éternel,
+\tD'annoncer le matin ta bonté.
+\tTu me réjouis par tes œuvres, ô Éternel !
+\tJe chante avec allégresse l'ouvrage de Tes mains.
+
+Que tes œuvres sont grandes et tes pensées profondes.
+L'homme stupide n'y connaît rien, l'insensé n'y prend point garde.
+Si les méchants croissent comme l'herbe et ceux qui font le mal fleurissent
+C'est pour être anéantis. Mais Toi, tu es le Très-Haut.
+
+Plantés dans la maison de Dieu, les justes, comme le palmier,
+Croissent dans les parvis de Dieu, verdoyants, sont pleins de sève,
+Et dans leur vieillesse portent des fruits. Ils s'élèvent comme le cèdre.
+C'est pour glorifier leur Dieu. L'Éternel est leur rocher.
+
+
+66
+Heureux les affligés
+Mt 5
+Heureux les affligés; ils seront consolés,
+Les pauvres en esprit; le royaume est à eux.
+Heureux les débonnaires; ils hériteront la terre.
+Heureux ceux qui ont faim et soif de la justice;
+Ils seront rassasiés !
+
+Heureux ceux qui procurent la vraie paix autour d'eux;.
+Ils seront appelés fils de notre Seigneur.
+Auront miséricorde les miséricordieux
+Heureux tous les cœurs purs car ils verront leur Dieu.
+Soyez dans l'allégresse !
+
+Heureux les méprisés; le ciel leur appartient !
+Et si l'on dit du mal sur vous à cause de Dieu,
+Soyez tous dans la joie et réjouissez-vous.
+Car votre récompense sera grande dans le ciel.
+Soyez tous dans la joie !
+
+Les prophètes avant vous aussi furent outragés.
+Si le sel de la terre et la lumière du monde
+Venaient à disparaître pour l'humanité,
+Que vos œuvres soient vues pour glorifier le Père !
+Oui, réjouissez-vous !
+
+
+67
+Vous avez été rachetés
+1 Co 6
+Vous avez été rachetés à un grand prix.
+Vous avez été rachetés à un grand prix.
+Au nom du Seigneur Jésus, vous avez été lavés,
+Sanctifiés, justifiés, justifiés.
+Glorifiez donc Dieu dans vos corps et votre esprit.
+Tous ceux qui s'attachent au Seigneur sont un en lui.
+Nos corps sont membres de Christ, car tout appartient à Dieu.
+Ne devenez pas esclaves des hommes !
+
+Pour nous, il n'y a qu'un seul Dieu, un seul Seigneur
+Qui a créé tout ce qui est, par qui nous sommes.
+Qu'as-tu que tu n'aies reçu, de quoi te glorifies-tu
+Puisqu'il nous a tirés de la poussière ?
+Et pourtant l'Esprit du Seigneur habite en nous;
+Nous ne nous appartenons plus, plus à nous-mêmes
+Soyons fermes dans la foi ! Faisons tout avec amour !
+Rendons grâces en toutes choses au Seigneur ! (bis)
+
+
+68
+Si tu donnes de ta subsistance
+Es 58
+\tSi tu donnes de ta subsistance
+\tÀ celui qui meurt de faim,
+\tSi tu rassasies l'âme indigente,
+\tTa lumière se lèvera.
+\tL'Éternel sera toujours ton guide.
+\tIl rassasiera ton âme aride,
+\tTa source sera intarissable.
+
+Donne ton pain à celui qui a faim !
+Reçois dans ta maison les malheureux !
+Si tu vois un homme nu, couvre-le;
+Ne te détourne pas de ton prochain !
+
+Dénoue les liens de la méchanceté !
+Brise les chaînes de la servitude !
+Renvoie libres ceux qui sont opprimés;
+Éloigne-toi des gestes menaçants !
+
+Ta lumière poindra comme l'aurore !
+Ta guérison germera promptement !
+Ta justice marchera devant toi,
+La splendeur de Dieu t'accompagnera.
+
+
+69
+Avec quoi irai-je ?
+Mi 6;Ps 51
+Avec quoi irai-je devant l'Éternel,
+Et m'inclinerai-je devant le Très-Haut ?
+Me prosternerai-je avec des offrandes ?
+Donnerai-je pour mon crime un premier-né ? (bis)
+
+\tOn t'a fait connaître, ô homme, ce qui est bien.
+\tCe que l'Éternel demande de toi,
+\tC'est que tu pratiques la justice,
+\tQue tu aimes la miséricorde.
+\tAvec ton Dieu, marche humblement !
+
+Tu ne prends pas plaisir aux grands sacrifices.
+Tu n'agrées d'holocaustes pour pardonner.
+Le sacrifice à Dieu : c'est l'esprit brisé.
+Ne dédaigne pas, Seigneur, un cœur contrit ! (bis)
+
+
+70
+Ne crains pas (canon)
+Es 43
+A. Ne crains pas, ne crains pas,
+B. Je t'ai racheté !
+C. Ne crains pas, ne crains pas,
+D. Je ne t'abandonnerai pas !
+
+
+71
+Invoque-moi
+Ps 91;92
+Invoque-moi et je te répondrai.
+Puisque tu m'aimes, je te délivrerai.
+Je te protégerai, puisque tu connais mon nom !
+Et je te ferai voir, je te ferai voir mon salut !
+Invoque-moi et je te répondrai.
+Puisque tu m'aimes, je te délivrerai,
+Je te délivrerai et tu Me glorifieras.
+Oh, si tu m'écoutais, ton bonheur durerait toujours !
+
+\tIl est beau de louer l'Éternel,
+\tD'annoncer le matin sa bonté,
+\tSa fidélité pendant les nuits, pendant les nuits !
+\tJe chanterai tant que je vivrai
+\tLa bonté, l'amour de l'Éternel !
+\tJe célébrerai sa sainteté, sa sainteté !
+
+Enseigne-moi tes voies, ô Éternel;
+Je marcherai dans ta fidélité,
+Et dispose mon cœur à la crainte de ton nom !
+Je me confie en toi. En toi, mon Dieu, je ne crains rien.
+Je ne crains pas les terreurs de la nuit.
+Aucun fléau n'approchera de moi
+Car tu es mon secours, mon refuge chaque jour !
+Car tu es mon secours et mon refuge chaque jour !
+
+
+72
+Vous serez mes témoins
+Ac 1
+Vous serez mes témoins, a dit Jésus. (bis)
+Vous recevrez une puissance, le Saint-Esprit. (bis)
+Merci Seigneur, car j'étais perdu.
+Pour toi je veux vivre maintenant;
+Prends ma vie, Seigneur, oui prends-la ! (bis)
+
+Vous serez mes témoins, a dit Jésus. (bis)
+Et jusqu'au bout du monde vous parlerez de moi. (bis)
+Oui, je veux dire au monde entier
+Que tu m'as sauvé, m'as racheté.
+Envoie-moi, Seigneur, envoie-moi ! (bis)
+
+Nous sommes ses témoins, a dit Jésus. (bis)
+Et nous chantons au monde entier : Jésus est le Roi ! (bis)
+Il est le Sauveur et le Seigneur !
+Il a donné sa vie sur la croix.
+Tu peux l'accepter, viens à lui ! (bis)
+
+
+73
+Louez l’éternel, il est bon
+Ps 66
+\tLouez l’Éternel ! Il est bon.
+\tChantez la gloire de son nom !
+\tCélébrez-le par vos louanges !
+\tDites : \"Ô Dieu, tes œuvres sont grandes !\"
+
+La terre se prosterne et chante en ton nom,
+Chante en ton honneur, ta force et ta gloire.
+
+Venez, contemplez les œuvres de Dieu;
+Il est redoutable dans sa majesté.
+
+
+74
+Poussez vers le Seigneur
+Ps 100
+Poussez vers le Seigneur des cris de joie, (bis)
+Vous tous, habitants de la terre !
+Servez le Seigneur avec joie,
+Venez avec allégresse en sa présence !
+Sachez que le Seigneur est Dieu !
+C’est lui qui nous a faits et nous lui appartenons.
+Nous sommes son peuple et le troupeau de son pâturage.
+Oh, oh...
+Entrez dans ses portes avec des louanges, (bis)
+Dans ses parvis avec des cantiques !
+Célébrez-le, bénissez son nom !
+Car le Seigneur est bon, sa bonté dure toujours
+Et sa fidélité de génération en génération.
+Car le Seigneur est bon, sa bonté dure toujours
+Et sa fidélité de génération en génération.
+    ";
+
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function homeAction()
     {
         return $this->render("TimotheeModelBundle:Hymns:home.html.twig");
+    }
+
+
+    /**
+     * @Route("/fill-bdd", name="fillBDD")
+     */
+    public function fillBDDAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $tab = explode("\n\n\n", $this->hymns);
+        $hymns = [];
+
+        foreach ($tab as $hymn) {
+            $tab = explode("\n", $hymn);
+
+
+            if(!empty($tab) AND $tab[0] != "")
+            {
+                $num = trim($tab[0]);
+                $title = trim($tab[1]);
+                $ref = trim($tab[2]);
+                $text = "";
+
+                for ($i = 3; $i < sizeof($tab); $i++) { 
+                    $text .= trim(
+                        preg_replace("/\t/", "<span style='padding-left:15px'></span>", $tab[$i])."<br>"
+                    );
+                }
+
+                $hymn = new Hymn($title,$num,$ref,$text);
+                $em->persist($hymn);
+
+                array_push($hymns, $hymn);
+            }
+
+        }
+
+        dump($hymns); die;
+
+        // $em->flush();
+
+        return $this->redirectToRoute("home");
     }
 
     /**
@@ -37,13 +620,16 @@ class HymnsController extends Controller
         $formatted = [];
 
         foreach ($hymns as $hymn) {
-            $formatted = [
-                "id" => $hymn->getId(),
-                "title" => $hymn->getTitle(),
-                "num" => $hymn->getNum(),
-                "ref" => $hymn->getRef(),
-                "lyrics" => $hymn->getLyrics()
-            ];        
+            array_push(
+                $formatted,
+                [
+                    "id" => $hymn->getId(),
+                    "title" => $hymn->getTitle(),
+                    "num" => $hymn->getNum(),
+                    "ref" => $hymn->getRef(),
+                    "lyrics" => $hymn->getLyrics()
+                ]
+            );       
         }
 
         return new JsonResponse($formatted);
